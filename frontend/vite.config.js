aab2870a72fb33faf264d66b8fe9af2238414465
Sumitkumar1109 <sumitkumar2000sep@gmail.com>
2025-04-19 10:14:07 +0530

    feat\deployment issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // Ensures correct relative paths in deployed static files
   plugins: [react()],
+  build: {
+    outDir: 'dist' // optional, default is 'dist'
+  }
 })
